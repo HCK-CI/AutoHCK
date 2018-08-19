02a61ee3ca330d892efa45542dde0020a141ba49
Lior Haim <lior@daynix.com>
2018-08-19 10:21:42 +0300

    Updating rtoolsHCK JSON output as default
    Signed-off-by: Lior Haim <lior@daynix.com>

--- lib/tools.rb
+++ lib/tools.rb
@@ -34,75 +34,72 @@ class Tools < RToolsHCK
   end
 
   def create_pool(tag)
-    handle_results(@tools.create_pool(tag, :json))
+    handle_results(@tools.create_pool(tag))
   end
 
   def create_project(tag)
-    handle_results(@tools.create_project(tag, :json))
+    handle_results(@tools.create_project(tag))
   end
 
   def list_pools
-    handle_results(@tools.list_pools(:json))
+    handle_results(@tools.list_pools)
   end
 
   def create_project_target(key, tag, machine)
-    handle_results(@tools.create_project_target(key, tag, machine, tag, :json))
+    handle_results(@tools.create_project_target(key, tag, machine, tag))
   end
 
   def list_machine_targets(machine, pool)
-    handle_results(@tools.list_machine_targets(machine, pool, :json))
+    handle_results(@tools.list_machine_targets(machine, pool))
   end
 
   def delete_machine(machine, pool)
-    handle_results(@tools.delete_machine(machine, pool, :json))
+    handle_results(@tools.delete_machine(machine, pool))
   end
 
   def restart_machine(machine)
-    handle_results(@tools.machine_shutdown(machine, :restart, :json))
+    handle_results(@tools.machine_shutdown(machine, :restart))
   end
 
   def shutdown_machine(machine)
-    handle_results(@tools.machine_shutdown(machine, :shutdown, :json))
+    handle_results(@tools.machine_shutdown(machine, :shutdown))
   end
 
   def shutdown
-    handle_results(@tools.shutdown(false, :json))
+    handle_results(@tools.shutdown(false))
   end
 
   def move_machine(machine, from, to)
-    handle_results(@tools.move_machine(machine, from, to, :json))
+    handle_results(@tools.move_machine(machine, from, to))
   rescue StandardError => e
     @logger.error("#{e.class}: #{e}")
   end
 
   def set_machine_ready(machine, pool)
-    handle_results(@tools.set_machine_state(machine, pool, 'ready', nil, :json))
+    handle_results(@tools.set_machine_state(machine, pool, 'ready'))
   rescue StandardError => e
     @logger.error("#{e.class}: #{e}")
   end
 
   def install_machine_driver_package(machine, method, driver_path, file)
     handle_results(@tools.install_machine_driver_package(machine, driver_path,
-                                                         method, file, :json))
+                                                         method, file))
   end
 
   def list_tests(key, machine, tag, playlist)
     handle_results(@tools.list_tests(key, tag, machine, tag, nil, nil,
-                                     playlist,
-                                     :json))
+                                     playlist))
   end
 
   def queue_test(test_id, target_key, machine, tag, support)
     handle_results(@tools.queue_test(test_id, target_key, tag, machine, tag,
-                                     support,
-                                     nil,
-                                     :json))
+                                     support))
   end
 
   def zip_test_result_logs(test_id, target_key, machine, tag)
     test_index = -1
     handle_results(@tools.zip_test_result_logs(test_index, test_id, target_key,
-                                               tag, machine, tag, :json))
+                                               tag, machine, tag))
   rescue StandardError
     @logger.info('Archiving tests results failed, trying again.')
     sleep 10
@@ -110,6 +107,6 @@ class Tools < RToolsHCK
   end
 
   def create_project_package(project, handler = nil)
-    handle_results(@tools.create_project_package(project, handler, :json))
+    handle_results(@tools.create_project_package(project, handler))
   end
 end
