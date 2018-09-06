3fc2039be1c1696b3423ca6b82ad13bc61d79499
Lior Haim <lior@daynix.com>
2018-09-06 14:37:45 +0300

    Checking the driver diff file and running the tests if needed diff_checker will look for the diff.txt file provided as argument or located in the provided driver folder, compare it with triggers.yml file rule that should decide if the test should or shouldn't run.
    Signed-off-by: Lior Haim <lior@daynix.com>

---
+++ lib/diff_checker.rb
@@ -0,0 +1,41 @@
+require 'yaml'
+
+# selective trigger class
+class DiffChecker
+  DIFF_FILENAME = 'diff.txt'.freeze
+  TRIGGER_YAML = 'triggers.yml'.freeze
+
+  def initialize(logger, device, driver_path, diff)
+    @logger = logger
+    @device = device['short']
+    @diff = diff || driver_path + '/' + DIFF_FILENAME
+  end
+
+  def load_device_triggers
+    @logger.info('Loading diff checker trigger file')
+    yaml = YAML.safe_load(File.read(TRIGGER_YAML))
+    yaml.select! { |_key, value| value ? value & [@device, '*'] != [] : false }
+    yaml.keys
+  end
+
+  def load_diff_files
+    @logger.info('Loading device diff file')
+    File.readlines(@diff)
+  end
+
+  def root_trigger?(triggers, files)
+    triggers.include?('/') && files.any? { |file| !file.include?('/') }
+  end
+
+  def subdir_trigger?(triggers, files)
+    files.any? { |line| triggers.any? { |trigger| line.start_with?(trigger) } }
+  end
+
+  def trigger?
+    return true unless File.file?(@diff) && File.file?(TRIGGER_YAML)
+
+    files = load_diff_files
+    triggers = load_device_triggers
+    root_trigger?(triggers, files) || subdir_trigger?(triggers, files)
+  end
+end
