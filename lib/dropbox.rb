8782ecf37c38bb0132628993554c2bbdc668f0d9
Bishara AbuHattoum <bishara@daynix.com>
2018-12-04 15:00:48 +0200

    Merging DropboxAPI class into Dropbox class
    Signed-off-by: Bishara AbuHattoum <bishara@daynix.com>

--- lib/dropbox.rb
+++ lib/dropbox.rb
@@ -1,4 +1,4 @@
-require './lib/dropbox_api'
+require 'dropbox_api'
 
 # dropbox class
 class Dropbox
@@ -8,27 +8,42 @@ class Dropbox
     @logger = project.logger
     @timestamp = project.timestamp
     @tag = project.tag
-    token ? @dropbox = DropboxAPI.new(token) : return
-    @logger.error('Dropbox authentication failure') unless @dropbox.connected?
+    @url = nil
+    token ? @dropbox = DropboxApi::Client.new(token) : return
+    @dropbox.get_current_account
+    @logger.error('Dropbox authentication failure') if @dropbox.nil?
     create_shared_folder
+  rescue DropboxApi::Errors::HttpError
+    @dropbox = nil
   end
 
   def create_shared_folder
-    return unless @dropbox && @dropbox.connected?
+    return unless @dropbox && !@dropbox.nil?
 
-    @url = @dropbox.create_folder("#{@tag}-#{@timestamp}")
+    @path = '/' + @tag + '-' + @timestamp
+    @dropbox.create_folder(@path)
+    @dropbox.share_folder(@path)
+    @url = @dropbox.create_shared_link_with_settings(@path).url + '&lst='
     @logger.info("Dropbox shared folder: #{@url}")
   end
 
-  def upload(file_path, file_name = nil)
-    return unless @dropbox && @dropbox.connected?
+  def upload(local_path, rename = nil)
+    return unless @dropbox && !@dropbox.nil? && @url
 
-    @dropbox.upload_file(file_path, file_name)
+    file_name = if rename.nil?
+                  File.basename(local_path)
+                else
+                  rename + File.extname(local_path)
+                end
+    file_content = IO.read(local_path)
+    remote_path = @path + '/' + file_name
+    @dropbox.upload(remote_path, file_content)
   end
 
   def upload_text(content, file_name)
-    return unless @dropbox && @dropbox.connected?
+    return unless @dropbox && !@dropbox.nil? && @url
 
-    @dropbox.upload_text(content, file_name)
+    remote_path = @path + '/' + file_name
+    @dropbox.upload(remote_path, content, mode: 'overwrite')
   end
 end
