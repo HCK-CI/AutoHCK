d08ced78409a604412c35bf5ab5084fce0613efc
Bishara AbuHattoum <bishara@daynix.com>
2018-12-04 17:26:12 +0200

    Abstraction for uploading the results
    To support more result uploaders like Dropbox in the future an
    abstraction should be implemented on the result uploading
    functionality, meaning that Project instance should have a
    ResultUploader instance that uses the ruby factory pattern to
    create uploaders instances as configured in "result_uploaders"
    array field in the condig.json file. At the moment only Dropbox
    is supported, so the config.json looks like this:
    
        "studio_username": "Administrator",
        "studio_password": "Qum5net.",
     -> "result_uploaders": [ "dropbox" ]
    }
    
    Uploader should implement these methods:
    
      initiailize            Initialize uploader instance
      connect                Connect if needed
      create_project_folder  Create a dedicated folder for the project
      upload_file            Upload a file to the project folder
      uploade_file_content   Update a file's content
      close                  Close the instance if needed
    
    See lib/dropbox.api for reference.
    
    Signed-off-by: Bishara AbuHattoum <bishara@daynix.com>

---
+++ lib/result_uploader.rb
@@ -0,0 +1,70 @@
+require './lib/dropbox'
+
+# ResultUploader
+#
+class ResultUploader
+  # UploaderFactory
+  #
+  class UploaderFactory
+    UPLOADERS = {
+      dropbox: Dropbox
+    }.freeze
+
+    def self.create(type, project)
+      UPLOADERS[type].new(project)
+    end
+
+    def self.can_create?(type)
+      !UPLOADERS[type].nil?
+    end
+  end
+
+  def initialize(project)
+    @project = project
+    @connected_uploaders = {}
+    @uploaders = {}
+    @project.config['result_uploaders'].uniq.collect(&:to_sym).each do |type|
+      if UploaderFactory.can_create?(type)
+        @uploaders[type] = UploaderFactory.create(type, @project)
+      else
+        @project.logger.info("Unknown type uploader #{type}, (ignoring)")
+      end
+    end
+  end
+
+  def connect
+    @uploaders.each_pair do |type, uploader|
+      if uploader.connect
+        @connected_uploaders[type] = uploader
+      else
+        @project.logger.info("#{type} connection failed, (ignoring)")
+      end
+    end
+  end
+
+  def create_project_folder
+    @connected_uploaders.each_value(&:create_project_folder)
+  end
+
+  def upload_file(l_path, r_name)
+    @connected_uploaders.each_value do |uploader|
+      uploader.upload_file(l_path, r_name)
+    end
+  end
+
+  def update_file_content(content, r_name)
+    @connected_uploaders.each_value do |uploader|
+      uploader.update_file_content(content, r_name)
+    end
+  end
+
+  def url
+    return nil unless @connected_uploaders.key?(:dropbox)
+
+    @connected_uploaders[:dropbox].url
+  end
+
+  def close
+    @connected_uploaders.each_value(&:close)
+  end
+end
