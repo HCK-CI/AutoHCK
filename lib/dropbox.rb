29957228797f8f61e2231e532ec6b909fcb924a6
Bishara AbuHattoum <bishara@daynix.com>
2019-06-03 09:31:36 -0400

    Dropbox class updates
     1. Better exception logging, warn level instead of error level.
     2. Prevent fatal exceptions which leads to failing the AutoHCK run.
     2. Re-establish connection when a connection loss is encoutered.
    
    Signed-off-by: Bishara AbuHattoum <bishara@daynix.com>

--- lib/dropbox.rb
+++ lib/dropbox.rb
@@ -14,59 +14,70 @@ class Dropbox
     @url = nil
   end
 
+  def handle_exceptions(where)
+    if @dropbox.nil?
+      @logger.warn("Dropbox connection error, ignoring #{where}")
+    else
+      yield
+    end
+  rescue Faraday::ConnectionFailed
+    @logger.warn("Dropbox connection lost while #{where}")
+    @logger.info('Trying to re-establish the Dropbox connection')
+    connect
+    retry
+  rescue StandardError => e
+    @logger.warn("Dropbox #{where} error: (#{e.class}) #{e.message}")
+  end
+
   def connect
     if @token
       @dropbox = DropboxApi::Client.new(@token)
-      @logger.error('Dropbox authentication failure') if @dropbox.nil?
+      @logger.warn('Dropbox authentication failure') if @dropbox.nil?
     else
       @logger.info('Dropbox token missing')
     end
     @dropbox.nil? ? false : true
   rescue DropboxApi::Errors::HttpError
-    @logger.error('Dropbox connection error')
+    @logger.warn('Dropbox connection error')
     false
   end
 
   def create_project_folder
-    return if @dropbox.nil?
-
-    @path = '/' + @tag + '-' + @timestamp
-    @dropbox.create_folder(@path)
-    @dropbox.share_folder(@path)
-    @url = @dropbox.create_shared_link_with_settings(@path).url + '&lst='
-    @logger.info("Dropbox project folder created: #{@url}")
-  rescue StandardError => e
-    @logger.error("Dropbox create_project_folder error: #{e.message}")
+    handle_exceptions(__method__) do
+      @path = '/' + @tag + '-' + @timestamp
+      @dropbox.create_folder(@path)
+      @dropbox.share_folder(@path)
+      @url = @dropbox.create_shared_link_with_settings(@path).url + '&lst='
+      @logger.info("Dropbox project folder created: #{@url}")
+    end
   end
 
   def upload_file(l_path, r_name)
-    return if @dropbox.nil?
-
-    content = IO.read(l_path)
-    r_path = @path + '/' + r_name
-    @dropbox.upload(r_path, content)
-  rescue StandardError => e
-    @logger.error("Dropbox upload_file error: #{e.message}")
+    handle_exceptions(__method__) do
+      content = IO.read(l_path)
+      r_path = @path + '/' + r_name
+      @dropbox.upload(r_path, content)
+    end
   end
 
   def update_file_content(content, r_name)
-    return if @dropbox.nil?
-
-    r_path = @path + '/' + r_name
-    @dropbox.upload(r_path, content, mode: 'overwrite')
-  rescue StandardError => e
-    @logger.error("Dropbox update_file_content error: #{e.message}")
+    handle_exceptions(__method__) do
+      r_path = @path + '/' + r_name
+      @dropbox.upload(r_path, content, mode: 'overwrite')
+    end
   end
 
   def delete_file(r_name)
-    return false if @dropbox.nil?
-
-    r_path = @path + '/' + r_name
-    @dropbox.delete(r_path)
-    @logger.info("Dropbox file deleted: #{r_path}")
-    true
-  rescue DropboxApi::Errors::NotFoundError
-    false
+    handle_exceptions(__method__) do
+      begin
+        r_path = @path + '/' + r_name
+        @dropbox.delete(r_path)
+        @logger.info("Dropbox file deleted: #{r_path}")
+        true
+      rescue DropboxApi::Errors::NotFoundError
+        false
+      end
+    end
   end
 
   def close; end
