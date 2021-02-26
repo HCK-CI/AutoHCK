2308e013493c4f9b45772c7e0e05d3d080494685
Kostiantyn Kostiuk <konstantin@daynix.com>
2021-02-26 13:23:12 +0000

    hckinstall: Add setup scripts helper
    This helper will be used for generating the config file with the
    proper test kit type and HLK kit version.
    
    Signed-off-by: Kostiantyn Kostiuk <konstantin@daynix.com>

---
+++ lib/engines/hckinstall/setup_scripts_helper.rb
@@ -0,0 +1,45 @@
+# frozen_string_literal: true
+
+require './lib/exceptions'
+
+# AutoHCK module
+module AutoHCK
+  # Helper module
+  module Helper
+    SUPPORTED_CONFIG = {
+      kit_type: '',
+      hlk_kit_ver: '',
+      remove_gui: '',
+      debug: ''
+    }.freeze
+
+    def validate_setup_scripts_config(config)
+      (config.keys - SUPPORTED_CONFIG.keys).each do |cfg|
+        raise(AutoHCKError, "Undefined HLK setup scripts config #{cfg.inspect}")
+      end
+    end
+
+    def create_setup_scripts_config(hck_setup_scripts_path, config)
+      validate_setup_scripts_config(config)
+
+      args_file = File.open("#{hck_setup_scripts_path}/args.ps1", 'w')
+      config.each do |k, v|
+        key = k.to_s.upcase.gsub(/[^0-9a-z]/i, '')
+        case v
+        when true, false
+          value = "$#{v}"
+        when String
+          value = "'#{v}'"
+        when Integer
+          value = v
+        else
+          @logger.fatal("Unexpected value #{x} for config")
+          raise(AutoHCKError, "Unexpected value #{x} for config")
+        end
+
+        args_file.write("$#{key} = #{value}\n")
+      end
+      args_file.close
+    end
+  end
+end
