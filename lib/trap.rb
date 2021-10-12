521529cfd061c482ca45104b96dff684e27c7e4c
Kostiantyn Kostiuk <konstantin@daynix.com>
2021-10-12 11:56:48 +0000

    Update traps mechanism
    Add trap for CI. In this case, a trap will be executed after
    the first signal.
    Add trap for SIGINT, to easily close AutoHCK when it runs
    manually from the console.
    
    Signed-off-by: Kostiantyn Kostiuk <konstantin@daynix.com>

--- lib/trap.rb
+++ lib/trap.rb
@@ -2,24 +2,64 @@
 
 # AutoHCK module
 module AutoHCK
-  @sigterm = false
-  Signal.trap('TERM') do
-    if @sigterm
-      @project&.logger&.warn('SIGTERM(2) received, aborting...')
-      Signal.trap('TERM') do
-        @project&.logger&.warn('SIGTERM(*) received, ignoring...')
+  # Trap class
+  class Trap
+    class << self
+      attr_writer :project
+    end
+
+    def self.clean_threads
+      Thread.list.each do |thread|
+        thread.exit unless Thread.main.eql?(thread)
+      end
+    end
+
+    def self.write_log(msg)
+      if @project.nil? || @project.logger.nil?
+        puts(msg)
+      else
+        @project.logger.warn(msg)
+      end
+    end
+
+    def self.perform_trap(signal)
+      Signal.trap(signal) do
+        write_log("SIG#{signal}(*) received, ignoring...")
       end
       @project&.handle_cancel
       @project&.close
       clean_threads
       exit
-    else
-      @sigterm = true
-      @project&.logger&.warn('SIGTERM(1) received, aborting if another SIGTERM is'\
-                          ' received in the span of the next one second')
-      Thread.new do
-        sleep 1
-        @sigterm = false
+    end
+
+    @sig_status = {}
+    def self.normal_trap(signal)
+      if @sig_status[signal]
+        write_log("SIG#{signal}(2) received, aborting...")
+        perform_trap(signal)
+      else
+        @sig_status[signal] = true
+        write_log("SIG#{signal}(1) received, aborting if another SIG#{signal} is"\
+                            ' received in the span of the next one second')
+        Thread.new do
+          sleep 1
+          @sig_status[signal] = false
+        end
+      end
+    end
+
+    def self.ci_trap(signal)
+      @sig_status[signal] = true
+      write_log("SIG#{signal}(1) received from CI, aborting ...")
+      perform_trap(signal)
+    end
+
+    def self.init_traps(signals)
+      @project = nil
+
+      signals.each do |signal|
+        @sig_status[signal] = false
+        Signal.trap(signal) { ENV['CI'].nil? ? normal_trap(signal) : ci_trap(signal) }
       end
     end
   end
