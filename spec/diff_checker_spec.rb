cbb42be2cb62b4d16089650d21aae13c927058ad
Kostiantyn Kostiuk <konstantin@daynix.com>
2021-11-15 10:07:28 +0000

    spec: Add tests for diff checker
    Signed-off-by: Kostiantyn Kostiuk <konstantin@daynix.com>

---
+++ spec/diff_checker_spec.rb
@@ -0,0 +1,19 @@
+require_relative '../lib/auxiliary/diff_checker'
+
+describe 'diff_checker' do
+  triggers = 'spec/diff_checker_spec/triggers.yml'
+  diffs = 'spec/diff_checker_spec/diff_*.txt'
+
+  Dir[diffs].each do |diff|
+    name = File.basename(diff)
+    name_parts = name.split('_')
+
+    expect_res = name_parts[3].split('.')[0] == 'pass'
+    drivers = [{ 'short' => name_parts[2] }]
+
+    it name.to_s do
+      dc = AutoHCK::DiffChecker.new(nil, drivers, '.', diff, triggers)
+      expect(dc.trigger?).to eq(expect_res)
+    end
+  end
+end
