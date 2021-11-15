95966a8608754975d448bf85c1df86d42e4c2b8c
Kostiantyn Kostiuk <konstantin@daynix.com>
2021-11-15 10:00:01 +0000

    Update triggers logic
    Add support for includes and excludes.
    
    Signed-off-by: Kostiantyn Kostiuk <konstantin@daynix.com>

--- lib/auxiliary/diff_checker.rb
+++ lib/auxiliary/diff_checker.rb
@@ -9,38 +9,95 @@ module AutoHCK
     DIFF_FILENAME = 'diff.txt'
     TRIGGER_YAML = 'triggers.yml'
 
-    def initialize(logger, drivers, driver_path, diff)
+    def initialize(logger, drivers, driver_path, diff = nil, triggers = nil)
       @logger = logger
       @drivers = drivers.map { |d| d['short'] }
       @diff = diff || "#{driver_path}/#{DIFF_FILENAME}"
+      @triggers = triggers || TRIGGER_YAML
     end
 
     def load_drivers_triggers
-      @logger.info('Loading diff checker trigger file')
-      yaml = YAML.safe_load(File.read(TRIGGER_YAML))
-      yaml.select! { |_key, value| value ? value & [*@drivers, '*'] != [] : false }
-      yaml.keys
+      @logger&.info('Loading diff checker trigger file')
+      yaml = YAML.safe_load(File.read(@triggers))
+
+      @trigger_includes = []
+      @trigger_excludes = []
+
+      yaml.each do |key, value|
+        if [*@drivers, '*'].include?(key)
+          @trigger_includes << value['include']
+          @trigger_excludes << value['exclude']
+        end
+      end
+
+      normalize_lists
+
+      @logger&.debug("Loaded trigger includes: #{@trigger_includes}")
+      @logger&.debug("Loaded trigger excludes: #{@trigger_excludes}")
+    end
+
+    def normalize_lists
+      @trigger_includes.flatten!
+      @trigger_includes.compact!
+      @trigger_includes.uniq!
+
+      @trigger_excludes.flatten!
+      @trigger_excludes.compact!
+      @trigger_excludes.uniq!
+    end
+
+    def check_trigger_file(trigger, line)
+      if trigger[-1] == '/'
+        # trigger is a directory
+        # applying for all files
+        line.start_with?(trigger)
+      else
+        # trigger is a file
+        line == trigger
+      end
     end
 
     def load_diff_files
-      @logger.info('Loading driver diff file')
-      File.readlines(@diff)
+      @logger&.info('Loading driver diff file')
+
+      @files = File.readlines(@diff, chomp: true)
+      @logger&.debug("Loaded driver diff files: #{@files}")
+
+      @files_no_excludes = @files.reject do |line|
+        @trigger_excludes.any? do |trigger|
+          check_trigger_file(trigger, line)
+        end
+      end
+      @logger&.debug("Driver diff files w/o excludes: #{@files_no_excludes}")
     end
 
-    def root_trigger?(triggers, files)
-      triggers.include?('/') && files.any? { |file| !file.include?('/') }
+    def root_trigger?
+      @logger&.debug('Processing root triggers')
+
+      return false unless @trigger_includes.include?('/')
+
+      @files_no_excludes.any? { |file| !file.include?('/') }
     end
 
-    def subdir_trigger?(triggers, files)
-      files.any? { |line| triggers.any? { |trigger| line.start_with?(trigger) } }
+    def subdir_trigger?
+      @files_no_excludes.any? do |line|
+        @trigger_includes.any? do |trigger|
+          check_trigger_file(trigger, line)
+        end
+      end
     end
 
     def trigger?
-      return true unless File.file?(@diff) && File.file?(TRIGGER_YAML)
+      return true unless File.file?(@diff) && File.file?(@triggers)
 
-      files = load_diff_files
-      triggers = load_drivers_triggers
-      root_trigger?(triggers, files) || subdir_trigger?(triggers, files)
+      load_drivers_triggers
+      load_diff_files
+
+      root_tr = root_trigger?
+      sub_tr = subdir_trigger?
+      @logger&.debug("Triggers results: root_trigger? = #{root_tr}, subdir_trigger? = #{sub_tr}")
+
+      root_tr || sub_tr
     end
   end
 end
