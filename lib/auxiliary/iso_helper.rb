335f270224a85a13d11b6c9e9bff275b6968efa5
Kostiantyn Kostiuk <konstantin@daynix.com>
2023-05-01 11:33:50 -0400

    ISO Helper: Use xorriso instead of mkisofs
    mkisofs is unavaliable in RHEL
    
    Signed-off-by: Kostiantyn Kostiuk <konstantin@daynix.com>

--- lib/auxiliary/iso_helper.rb
+++ lib/auxiliary/iso_helper.rb
@@ -7,10 +7,10 @@ module AutoHCK
   # Helper module
   module Helper
     def create_iso(iso_path, dir_paths, exclude_list = [])
-      exclude_args = exclude_list.map { |v| "-exclude=#{v}" }
+      exclude_args = exclude_list.map { |v| "-old-exclude #{v}" }
 
       run_cmd([
-        'mkisofs -iso-level 4 -l -R -udf -D -allow-limited-size',
+        'xorriso -as mkisofs  -iso-level 4 -J -l -D -N -joliet-long -relaxed-filenames -V "INSTALLER"',
         *exclude_args, '-o', iso_path, *dir_paths
       ].join(' '))
     end
