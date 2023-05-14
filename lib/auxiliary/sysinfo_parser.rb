3ac6ada9b23665b030422c0b139559bf947ceb1f
Kostiantyn Kostiuk <konstantin@daynix.com>
2023-05-14 11:11:06 -0400

    Add systeminfo parser
    The code was written by ChatGPT based on
    the idea of https://github.com/kellyjonbrazil/jc.
    Some parser features are missing because they
    are redundant for HCK-CI.
    
    Signed-off-by: Kostiantyn Kostiuk <konstantin@daynix.com>

---
+++ lib/auxiliary/sysinfo_parser.rb
@@ -0,0 +1,43 @@
+# frozen_string_literal: true
+
+require 'csv'
+
+# AutoHCK module
+module AutoHCK
+  # SysInfoParser class
+  class SysInfoParser
+    # Main text parsing function
+    #
+    # @param data [String] Text data to parse
+    #
+    # @return [Hash] Dictionary with processed structured data.
+    def parse(data)
+      CSV.parse(data, headers: true).first.each.to_h do |k, v|
+        [
+          k,
+          case k
+          when 'Hotfix(s)', 'Processor(s)'
+            parse_hotfixs_or_processors v
+          else
+            v.strip
+          end
+        ]
+      end
+    end
+
+    private
+
+    # Turns a list of return-delimited hotfixes or processors
+    # with [x] as a prefix into an array of hotfixes
+    #
+    # Parameters:
+    #   data: (string) Input string
+    def parse_hotfixs_or_processors(data)
+      arr_output = data.split(',')
+      # skip line that says how many are installed
+      arr_output.shift
+      # discard the number sequence
+      arr_output.map! { _1.split(':')[1].strip }
+    end
+  end
+end
