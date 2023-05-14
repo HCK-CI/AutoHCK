e48ede4f9b22f882a0bb0b246c0a4dae065ff0a4
Kostiantyn Kostiuk <konstantin@daynix.com>
2023-05-14 11:17:10 -0400

    Add systeminfo parser tests
    Signed-off-by: Kostiantyn Kostiuk <konstantin@daynix.com>

---
+++ spec/sysinfo_parser_spec.rb
@@ -0,0 +1,22 @@
+require 'json'
+require_relative '../lib/auxiliary/sysinfo_parser'
+
+describe 'sysinfo_parser' do
+  spec_path = 'spec/sysinfo_parser_spec'
+  sysinfos = "#{spec_path}/sysinfo_*.txt"
+
+  Dir[sysinfos].each do |sysinfo|
+    name = File.basename(sysinfo)
+    json_name = "#{spec_path}/#{name.split('.')[0]}.json"
+
+    data = File.read(sysinfo)
+    json_data = JSON.parse(File.read(json_name))
+
+    parser = AutoHCK::SysInfoParser.new
+
+    it name.to_s do
+      result = parser.parse(data)
+      expect(result).to eq(json_data)
+    end
+  end
+end
