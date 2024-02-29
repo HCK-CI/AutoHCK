1785d0bb70fc985a4e8cef38252e775e0b864031
Kostiantyn Kostiuk <konstantin@daynix.com>
2024-02-29 17:42:44 +0200

    Define driver struct
    Signed-off-by: Kostiantyn Kostiuk <konstantin@daynix.com>

---
+++ lib/models/driver.rb
@@ -0,0 +1,41 @@
+# typed: strict
+# frozen_string_literal: true
+
+require 'sorbet-runtime'
+
+require_relative 'json_helper'
+require_relative 'command_info'
+require_relative 'driver_install_methods'
+
+# AutoHCK module
+module AutoHCK
+  # Models module
+  module Models
+    # Driver class
+    class Driver < T::Struct
+      extend T::Sig
+      extend JsonHelper
+
+      prop :short, T.nilable(String)
+
+      const :name, String
+      const :device, String
+      const :install_method, DriverInstallMethods
+      const :type, Integer
+      const :support, T::Boolean
+
+      const :inf, T.nilable(String)
+      const :sys, T.nilable(String)
+      const :install_cert, T.nilable(T::Boolean)
+      const :install_command, T.nilable(String)
+      const :s3_state, T.nilable(T::Boolean)
+      const :s4_state, T.nilable(T::Boolean)
+      const :enlightenments_state, T.nilable(T::Boolean)
+
+      const :pretestcommands, T.nilable(T::Array[CommandInfo])
+      const :extra_software, T.nilable(T::Array[String])
+      const :reject_test_names, T.nilable(T::Array[String])
+      const :select_test_names, T.nilable(T::Array[String])
+    end
+  end
+end
