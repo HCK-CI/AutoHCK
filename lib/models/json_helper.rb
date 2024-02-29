25e6c8e4bc92a00a3e7285aeb9ecbdeaf19f5b53
Kostiantyn Kostiuk <konstantin@daynix.com>
2024-02-29 17:36:34 +0200

    Define JSON loading helper for Sorbet structs
    Signed-off-by: Kostiantyn Kostiuk <konstantin@daynix.com>

---
+++ lib/models/json_helper.rb
@@ -0,0 +1,28 @@
+# typed: strict
+# frozen_string_literal: true
+
+require 'sorbet-runtime'
+
+require './lib/auxiliary/json_helper'
+
+module AutoHCK
+  # Models module
+  module Models
+    # Helper module
+    module JsonHelper
+      extend T::Sig
+      extend T::Generic
+      abstract!
+
+      has_attached_class!
+
+      sig { abstract.params(hash: T::Hash[String, T.untyped]).returns(T.attached_class) }
+      def from_hash(hash); end
+
+      sig { params(json_file: String, logger: T.nilable(T.any(MultiLogger, ::Logger))).returns(T.attached_class) }
+      def from_json_file(json_file, logger = nil)
+        from_hash(AutoHCK::Helper::Json.read_json(json_file, logger))
+      end
+    end
+  end
+end
