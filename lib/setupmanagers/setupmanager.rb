a83b07c71dd9480ed2d5b7e15c7a2136eb1e1e32
Akihiko Odaki <akihiko.odaki@daynix.com>
2024-06-10 15:40:20 +0900

    Simplify Engine and SetupManager
    They are effectively stateless so there is no point of having a class.
    
    Signed-off-by: Akihiko Odaki <akihiko.odaki@daynix.com>

--- lib/setupmanagers/setupmanager.rb
+++ lib/setupmanagers/setupmanager.rb
@@ -6,42 +6,17 @@ require './lib/setupmanagers/qemuhck/qemuhck'
 
 # AutoHCK module
 module AutoHCK
-  # SetupManager
-  #
-  class SetupManager < SimpleDelegator
-    attr_reader :id
+  module SetupManager
+    SETUP_MANAGERS = {
+      qemuhck: QemuHCK,
+      physhck: PhysHCK
+    }.freeze
 
-    # SetupManagerFactory
-    #
-    class SetupManagerFactory
-      SETUP_MANAGERS = {
-        qemuhck: QemuHCK,
-        physhck: PhysHCK
-      }.freeze
+    def self.select(name)
+      type = SETUP_MANAGERS[name.downcase.to_sym]
+      raise SetupManagerError, "Unknown type setup manager #{name}" if type.nil?
 
-      def self.create(type, project)
-        SETUP_MANAGERS[type].new(project)
-      end
-
-      def self.can_create?(type)
-        !SETUP_MANAGERS[type].nil?
-      end
-    end
-
-    def initialize(project)
-      @project = project
-      @logger = project.logger
-      @type = project.engine.platform['setupmanager'].downcase.to_sym
-      super(setupmanager_create)
-    end
-
-    def setupmanager_create
-      if SetupManagerFactory.can_create?(@type)
-        SetupManagerFactory.create(@type, @project)
-      else
-        @logger.warn("Unkown type setup manager #{@type}, Exiting...")
-        raise SetupManagerError, "Unkown type setup manager #{@type}"
-      end
+      type
     end
   end
 end
