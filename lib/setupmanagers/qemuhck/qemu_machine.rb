8c469cdd7750690e0013c3d4181244ba803c0407
Akihiko Odaki <akihiko.odaki@daynix.com>
2024-06-10 18:15:05 +0900

    Add bin/slirp
    Signed-off-by: Akihiko Odaki <akihiko.odaki@daynix.com>

--- lib/setupmanagers/qemuhck/qemu_machine.rb
+++ lib/setupmanagers/qemuhck/qemu_machine.rb
@@ -11,34 +11,31 @@ module AutoHCK
 
     # Hostfwd is a class that holds ports forwarded for a run.
     class Hostfwd
-      def initialize(slirp, ports)
-        @slirp = slirp
+      include Helper
+
+      def initialize(logger, workspace_path, ports)
+        @logger = logger
+        @workspace_path = workspace_path
         @ids = []
         begin
-          ports.each do |port|
-            @ids << slirp.run({
-                                'execute' => 'add_hostfwd',
-                                'arguments' => {
-                                  'proto' => 'tcp',
-                                  'host_port' => port,
-                                  'guest_port' => port
-                                }
-                              })
-          end
+          ports.each { @ids << slirp('add_hostfwd', _1.to_s)['id'] }
         rescue StandardError
           close
         end
       end
 
       def close
-        @ids.each do |id|
-          @slirp.run({
-                       'execute' => 'remove_hostfwd',
-                       'arguments' => id
-                     })
-        end
+        slirp 'remove_hostfwd', @ids.pop.to_s until @ids.empty?
+      end
 
-        @ids.clear
+      private
+
+      def slirp(*args)
+        File.open('/tmp', File::RDWR | File::TMPFILE) do |out|
+          run_cmd('bin/slirp', '-w', @workspace_path, *args, out:)
+          out.rewind
+          JSON.parse(out.read)['return']
+        end
       end
     end
 
@@ -705,7 +702,7 @@ module AutoHCK
         end
 
         scope.transaction do |tmp_scope|
-          hostfwd = Hostfwd.new(@options['slirp'], [@monitor_port, @vnc_port])
+          hostfwd = Hostfwd.new(@logger, @workspace_path, [@monitor_port, @vnc_port])
           tmp_scope << hostfwd
           Runner.new(tmp_scope, @logger, self, @run_name, @run_opts)
         end
