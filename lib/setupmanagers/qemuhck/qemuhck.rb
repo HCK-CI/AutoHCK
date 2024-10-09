6ce7c227133ccb1e73e662f184663eb8f94c03bc
Kostiantyn Kostiuk <konstantin@daynix.com>
2024-10-09 12:56:58 +0300

    Drop unused pluggable_memory_gb from driver
    Signed-off-by: Kostiantyn Kostiuk <konstantin@daynix.com>

--- lib/setupmanagers/qemuhck/qemuhck.rb
+++ lib/setupmanagers/qemuhck/qemuhck.rb
@@ -12,8 +12,7 @@ module AutoHCK
     attr_reader :kit, :project
 
     QEMUHCK_INFO_LOG_FILE = 'qemuhck.txt'
-    OPT_NAMES = %w[viommu_state s3_state s4_state enlightenments_state vhost_state machine_type fw_type cpu
-                   pluggable_memory_gb].freeze
+    OPT_NAMES = %w[viommu_state s3_state s4_state enlightenments_state vhost_state machine_type fw_type cpu].freeze
 
     def initialize(project)
       initialize_project project
