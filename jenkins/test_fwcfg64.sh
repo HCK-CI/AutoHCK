e1ac47b4b37dee35cd4fc16413bfe03c55b1c324
Kostiantyn Kostiuk <konstantin@daynix.com>
2025-02-10 19:28:23 +0200

    CI: Update platforms
    Signed-off-by: Kostiantyn Kostiuk <konstantin@daynix.com>

--- jenkins/test_fwcfg64.sh
+++ jenkins/test_fwcfg64.sh
@@ -7,7 +7,7 @@ test_name="$(basename "${0}" '.sh')"
 cd "${WORKSPACE}"
 
 bin/auto_hck --verbose test \
-    --platform Win2022x64_uefi \
+    --platform Win2022x64 \
     --drivers fwcfg64 \
     --driver-path "${VIRTIO_WIN_PATH}/amd64/2k22/" \
     --select-test-names "${WORKSPACE}/jenkins/sanity_tests" \
