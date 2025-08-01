fdfa7b39a9d701ec4589af4660810e541052f675
Kostiantyn Kostiuk <kkostiuk@redhat.com>
2025-08-01 13:23:49 +0300

    RHELMISC-15655: Update HLK kits
    Add sha256 for kits that provide online exe installer,
    because last time MS update kits without updating link.
    This cause problem that new kit silently was installed.
    
    Signed-off-by: Kostiantyn Kostiuk <kkostiuk@redhat.com>

--- spec/kit_spec.rb
+++ spec/kit_spec.rb
@@ -1,23 +1,28 @@
 require_relative '../lib/all'
+require 'digest'
 
 describe 'kit_spec' do
   Dir['./lib/engines/hckinstall/kits/*.json'].each do |json_file|
-    kit_url = AutoHCK::Models::Kit.from_json_file(json_file).download_url
+    kit = AutoHCK::Models::Kit.from_json_file(json_file)
+    kit_url = kit.download_url
     next if kit_url.nil?
 
     it "Kit for #{json_file} can be downloaded" do
       content = String.new
 
+      file_type = nil
       HTTPClient.get_content(kit_url) do |chunk|
         content << chunk
-        break if content.size > 32_773
+        file_type ||= 'exe' if content[0..1] == 'MZ'
+        # For ISOs, we only need the header. For EXEs, we need the whole file for checksum.
+        if content.size > 32_773 # Check for ISO header
+          file_type ||= 'iso' if content[32_769..32_773] == 'CD001'
+          break unless file_type == 'exe'
+        end
       end
 
-      file_type = nil
-      file_type = 'exe' if content[0..1] == 'MZ'
-      file_type = 'iso' if content[32_769..32_773] == 'CD001'
-
       expect(%w[exe iso]).to include(file_type)
+      expect(Digest::SHA256.hexdigest(content)).to eq(kit.sha256) if file_type == 'exe'
     end
   end
 end
