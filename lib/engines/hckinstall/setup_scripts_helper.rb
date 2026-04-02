7e187f592eb711cce3716795d5116ce0361fe005
Kostiantyn Kostiuk <kkostiuk@redhat.com>
2026-04-02 21:04:29 +0300

    hckinstall: Pathname-based setup script helpers
    Add copy_setup_scripts_template, use Pathname for kit download and
    args.ps1 paths, ensure Kits directory exists, and drop rm_rf of
    extra-software before copy.
    
    Signed-off-by: Konstantin Kostiuk <kkostiuk@redhat.com>

--- lib/engines/hckinstall/setup_scripts_helper.rb
+++ lib/engines/hckinstall/setup_scripts_helper.rb
@@ -12,6 +12,10 @@ module AutoHCK
       no_reboot_after_bugcheck: ''
     }.freeze
 
+    def copy_setup_scripts_template(workspace_hlk_setup_scripts_path, hck_setup_scripts_template_path)
+      FileUtils.copy_entry(hck_setup_scripts_template_path, workspace_hlk_setup_scripts_path)
+    end
+
     def validate_setup_scripts_config(config)
       extra_keys = (config.keys - SUPPORTED_CONFIG.keys)
       return if extra_keys.empty?
@@ -19,9 +23,11 @@ module AutoHCK
       raise(AutoHCKError, "Undefined HLK setup scripts configs: #{extra_keys.join(', ')}.")
     end
 
-    def download_kit_installer(url, kit, hck_setup_scripts_path)
+    def download_kit_installer(url, kit, workspace_hlk_setup_scripts_path)
       dw = Downloader.new(@logger)
-      kit_setup_path = "#{hck_setup_scripts_path}/Kits/#{kit}Setup"
+
+      FileUtils.mkdir_p(workspace_hlk_setup_scripts_path.join('Kits'))
+      kit_setup_path = workspace_hlk_setup_scripts_path.join('Kits', "#{kit}Setup")
       temp_path = "#{kit_setup_path}.tmp"
 
       dw.download(url, temp_path)
@@ -34,20 +40,20 @@ module AutoHCK
       kit_path
     end
 
-    def copy_extra_software(hck_setup_scripts_path, extra_software_path, sw_names)
-      FileUtils.rm_rf("#{hck_setup_scripts_path}/extra-software")
-      FileUtils.mkdir_p("#{hck_setup_scripts_path}/extra-software")
+    def copy_extra_software(workspace_hlk_setup_scripts_path, extra_software_path, sw_names)
+      workspace_extra_software_path = workspace_hlk_setup_scripts_path.join('extra-software')
+      FileUtils.mkdir_p(workspace_extra_software_path)
 
       sw_names.each do |name|
-        FileUtils.cp_r("#{extra_software_path}/#{name}",
-                       "#{hck_setup_scripts_path}/extra-software/#{name}")
+        FileUtils.cp_r(Pathname.new(extra_software_path).join(name),
+                       workspace_extra_software_path.join(name))
       end
     end
 
-    def create_setup_scripts_config(hck_setup_scripts_path, config)
+    def create_setup_scripts_config(workspace_hlk_setup_scripts_path, config)
       validate_setup_scripts_config(config)
 
-      File.open("#{hck_setup_scripts_path}/args.ps1", 'w') do |args_file|
+      File.open(workspace_hlk_setup_scripts_path.join('args.ps1'), 'w') do |args_file|
         config.each do |k, v|
           key = k.to_s.upcase.gsub(/[^0-9a-z]/i, '')
           case v
