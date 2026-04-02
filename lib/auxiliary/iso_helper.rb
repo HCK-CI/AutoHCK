2c984e2a8f92d887d9e00d15cc39fd64cdaa5e4e
Kostiantyn Kostiuk <kkostiuk@redhat.com>
2026-04-02 20:59:59 +0300

    auxiliary: Pathname-aware ISO creation and logging
    Teach create_iso to accept Pathname arguments (string paths for xorriso),
    add an info log of sources and excludes, and annotate with Sorbet.
    
    Signed-off-by: Konstantin Kostiuk <kkostiuk@redhat.com>

--- lib/auxiliary/iso_helper.rb
+++ lib/auxiliary/iso_helper.rb
@@ -1,14 +1,21 @@
+# type: true
 # frozen_string_literal: true
 
 # AutoHCK module
 module AutoHCK
   # Helper module
   module Helper
+    extend T::Sig
+
+    sig { params(iso_path: Pathname, dir_paths: T::Array[Pathname], exclude_list: T::Array[String]).void }
     def create_iso(iso_path, dir_paths, exclude_list = [])
       exclude_args = exclude_list.flat_map { ['-old-exclude', _1] }
 
+      dir_paths_strs = dir_paths.map(&:to_s)
+      @logger.info("Creating ISO image at #{iso_path} from #{dir_paths_strs} with excludes: #{exclude_list}")
+
       run_cmd(*%w[xorriso -as mkisofs -iso-level 4 -J -l -D -N -joliet-long -relaxed-filenames -V INSTALLER],
-              *exclude_args, '-o', iso_path, *dir_paths)
+              *exclude_args, '-o', iso_path.to_s, *dir_paths_strs)
     end
   end
 end
