4cbf5ed0d3df7c1f237565f8b2c1ea82c0c40935
Kostiantyn Kostiuk <kkostiuk@redhat.com>
2026-05-04 18:34:36 +0300

    Add RSpec for CmdRun
    Tagged :linux_process (real spawn). Covers success, shelljoin logging,
    CmdRunError, exception: false, idempotent close, chdir, and status
    after rescue.
    
    Signed-off-by: Kostiantyn Kostiuk <kkostiuk@redhat.com>

---
+++ spec/lib/auxiliary/cmd_run_spec.rb
@@ -0,0 +1,91 @@
+# frozen_string_literal: true
+
+require_relative '../../../lib/all'
+
+# rubocop:disable Metrics/BlockLength
+describe AutoHCK::CmdRun, :linux_process do
+  let(:logger_io) { StringIO.new }
+  let(:logger) { MonoLogger.new(logger_io) }
+
+  def new_cmd(scope, *args, **kwargs)
+    described_class.new(scope, logger, *args, **kwargs)
+  end
+
+  def wait_for_log_substring(substring, timeout: 2)
+    deadline = Process.clock_gettime(Process::CLOCK_MONOTONIC) + timeout
+    until logger_io.string.include?(substring)
+      if Process.clock_gettime(Process::CLOCK_MONOTONIC) >= deadline
+        raise RSpec::Expectations::ExpectationNotMetError,
+              "expected log to include #{substring.inspect} within #{timeout}s"
+      end
+      sleep 0.01
+    end
+  end
+
+  describe '#initialize and #close' do
+    it 'runs a successful argv-style command and reports exit 0' do
+      AutoHCK::ResourceScope.open([]) do |scope|
+        cmd = new_cmd(scope, '/bin/true')
+        expect(cmd.close.exitstatus).to be_zero
+      end
+      wait_for_log_substring('Run command:')
+      wait_for_log_substring('finished with code 0')
+    end
+
+    it 'records a multi-argument command using shelljoin in the log line' do
+      AutoHCK::ResourceScope.open([]) do |scope|
+        cmd = new_cmd(scope, '/bin/echo', 'hi')
+        cmd.close
+      end
+      joined = ['/bin/echo', 'hi'].shelljoin
+      wait_for_log_substring("Run command: #{joined}")
+    end
+
+    it 'raises CmdRunError when the command fails and exception is true' do
+      AutoHCK::ResourceScope.open([]) do |scope|
+        cmd = new_cmd(scope, '/bin/false')
+        expect { cmd.close }.to raise_error(AutoHCK::CmdRunError, /Failed to run \(PID/)
+        wait_for_log_substring('Run command:')
+      end
+    end
+
+    it 'does not raise when exception is false even if exit status is non-zero' do
+      AutoHCK::ResourceScope.open([]) do |scope|
+        cmd = new_cmd(scope, '/bin/false', exception: false)
+        expect(cmd.close.exitstatus).to eq(1)
+      end
+      wait_for_log_substring('finished with code 1')
+    end
+
+    it 'returns the same Process::Status on a second close' do
+      AutoHCK::ResourceScope.open([]) do |scope|
+        cmd = new_cmd(scope, '/bin/true')
+        first = cmd.close
+        expect(cmd.close).to equal(first)
+      end
+    end
+
+    it 'leaves status available after a rescued CmdRunError' do
+      AutoHCK::ResourceScope.open([]) do |scope|
+        cmd = new_cmd(scope, '/bin/false', exception: true)
+        expect { cmd.close }.to raise_error(AutoHCK::CmdRunError)
+        expect(cmd.close.exitstatus).to eq(1)
+      end
+    end
+  end
+
+  describe 'spawn options' do
+    it 'passes chdir through to spawn' do
+      Dir.mktmpdir('cmd_run_chdir') do |dir|
+        AutoHCK::ResourceScope.open([]) do |scope|
+          cmd = new_cmd(scope, '/bin/pwd', chdir: dir)
+          cmd.close
+        end
+        wait_for_log_substring('stdout:')
+        logged_dir = logger_io.string[/stdout: (.+?)(?:\n|$)/, 1]
+        expect(File.realpath(logged_dir)).to eq(File.realpath(dir))
+      end
+    end
+  end
+end
+# rubocop:enable Metrics/BlockLength
