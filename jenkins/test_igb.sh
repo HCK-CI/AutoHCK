ab86a81ae225e9bce6aa41844c7faa4d9e654045
Kostiantyn Kostiuk <kkostiuk@redhat.com>
2026-05-20 18:46:08 +0300

    Jenkins: Update platform names
    Signed-off-by: Kostiantyn Kostiuk <kkostiuk@redhat.com>

--- jenkins/test_igb.sh
+++ jenkins/test_igb.sh
@@ -7,7 +7,7 @@ test_name="$(basename "${0}" '.sh')"
 cd "${WORKSPACE}"
 
 bin/auto_hck --verbose test \
-    --platform Win2025x64_host \
+    --platform Win2025x64 \
     --drivers igb \
     --select-test-names "${WORKSPACE}/jenkins/sanity_tests" \
     --gthb_context_prefix "${test_name}: " \
